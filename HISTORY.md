de7bb67b3d190c0381e2d3ace1ff5f458f896a96
Rob Loach <robloach@gmail.com>
2015-10-16 21:55:56 -0400

    Update Boilerplate

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v0.1.0: 2015-10-15
+
+- Updated Boilerplate
+- Updated to `traceur@0.0.92`
+
 ## v0.0.1: 2015-08-16
 
 - Initial release
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var traceur = require('traceur')
-var extend = require('extend-shallow')
+var traceur = require('traceur');
+var extend = require('extend-shallow');
 
-exports.name = 'traceur'
-exports.inputFormats = ['es6', 'traceur']
-exports.outputFormat = 'js'
+exports.name = 'traceur';
+exports.inputFormats = ['es6', 'traceur'];
+exports.outputFormat = 'js';
 
 exports.render = function (str, options, locals) {
-  var opts = extend({}, options || {}, locals || {})
-  var sourceName = opts.sourceName || null
-  var outputName = opts.outputName || null
+  var opts = extend({}, options || {}, locals || {});
+  var sourceName = opts.sourceName || null;
+  var outputName = opts.outputName || null;
 
-  return traceur.compile(str, options, sourceName, outputName)
-}
+  return traceur.compile(str, options, sourceName, outputName);
+};
