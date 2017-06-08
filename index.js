98ffda854393491e97175e3ebf4b6b5f69142c4f
Rob Loach <robloach@gmail.com>
2017-06-08 10:45:43 -0400

    Fix coding standards

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict'
 
-var traceur = require('traceur')
-var extend = require('extend-shallow')
+const traceur = require('traceur')
+const extend = require('extend-shallow')
 
 exports.name = 'traceur'
 exports.inputFormats = ['es6', 'traceur']
 exports.outputFormat = 'js'
 
 exports.render = function (str, options, locals) {
-  var opts = extend({}, options || {}, locals || {})
-  var sourceName = opts.sourceName || null
-  var outputName = opts.outputName || null
+  const opts = extend({}, options || {}, locals || {})
+  const sourceName = opts.sourceName || null
+  const outputName = opts.outputName || null
 
   return traceur.compile(str, options, sourceName, outputName)
 }
